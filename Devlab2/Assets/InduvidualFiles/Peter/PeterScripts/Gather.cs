a946addd7e003578e267d9bb996ca32e7d8b8e32
gmd1apeterbos <97040824@st.deltion.nl>
2018-05-15 10:51:18 +0200

    Some stuff

--- Devlab2/Assets/InduvidualFiles/Peter/PeterScripts/Gather.cs
+++ Devlab2/Assets/InduvidualFiles/Peter/PeterScripts/Gather.cs
@@ -22,7 +22,7 @@ public class Gather : MonoBehaviour {
 
     public void OnTriggerEnter(Collider col)
     {
-        Debug.Log(col.name);
+        //Debug.Log(col.name);
 
         if(col.transform.tag == "Resource")
         {

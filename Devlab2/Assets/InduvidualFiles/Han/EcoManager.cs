a9054d1e312c506d86e22d03d0dc6878421a8b7c
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-05-22 17:01:47 +0200

    s

---
+++ Devlab2/Assets/InduvidualFiles/Han/EcoManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcoManager : MonoBehaviour {
+    public static EcoManager instance;
+
+    public enum GroundState {
+        Grass = 1,
+        Sand = 2,
+        Water = 3,
+        burned = 4,
+        fertile = 5,
+    }
+
+    public int xSize, ySize;
+    public float xStepSize, yStepSize;
+    public GameManager tile;
+    Tile[,] Grid;
+
+
+    public void GenerateMap() {
+        Grid = new Tile[xSize, ySize];
+
+        for(int x = 0; x < xSize; x++) {
+            for(int y = 0; y < ySize; y++) {
+
+                // spot = new Vector3(Mathf.);
+                //GameObject newTile = Instantiate(tile,new Vector3(x * xStepSize,))
+                //Grid[x, y] = new Tile(newTile);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class Tile {
+        public List<GroundState> myTimeLine = new List<GroundState>();
+        GameObject myTile;
+
+        public Tile(GameObject newTile) {
+            myTile = newTile;
+        }
+
+    }
+}
+
---
+++ Devlab2/Assets/InduvidualFiles/Han/EcoManagerEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EcoManager))]
+public class EcoManagerEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Generate Map")) {
+            EcoManager.instance.GenerateMap();
+        }
+    }
+}

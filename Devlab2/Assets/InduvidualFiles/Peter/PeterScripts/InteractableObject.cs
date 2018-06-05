329c4d636cfbe1955a0d7f9ee581869a292d8ad1
gmd1apeterbos <97040824@st.deltion.nl>
2018-06-05 16:25:12 +0200

    Update

---
+++ Devlab2/Assets/InduvidualFiles/Peter/PeterScripts/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Interact intr = other.GetComponent<Interact>();
+            StartInteract(intr);
+        }
+    }
+
+    public void StartInteract(Interact intr)
+    {
+        Interact.currentlyInteracting = this;
+        intr.craftingStationCanvas.enabled = true;
+        //xxxtra-logic.com right here
+
+    }
+}

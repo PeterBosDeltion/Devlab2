ac691ad3bb0052504df0738f8df1eba72dd78252
gmd1apeterbos <97040824@st.deltion.nl>
2018-06-07 10:22:26 +0200

    menu & building's borked

---
+++ Devlab2/Assets/InduvidualFiles/Peter/PeterScripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+
+    public void LoadNextIndexScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadSpecificIndexScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+   
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

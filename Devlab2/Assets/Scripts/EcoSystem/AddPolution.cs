6eacffd90c5e7d9b62c123eba5398538afff515d
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-06-24 20:30:03 +0200

    Big Inplomentoboy

--- Devlab2/Assets/Scripts/EcoSystem/AddPolution.cs
+++ Devlab2/Assets/Scripts/EcoSystem/AddPolution.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class AddPolution : MonoBehaviour {
     public int pollution;
-    public ParticleSystem myPartical;
+    public GameObject myPartical;
     public bool canBurn;
     bool burning;
     public int burnMin, burnMax;
     public Resource myResource;
     public Item burnedResource;
+    public Renderer myRenderer;
+    public GameObject toDestroy;
 
     void OnEnable() {
-        if(EcoManager.instance != null) {
+        if (EcoManager.instance != null) {
             EcoManager.instance.AddPollution(pollution);
             EcoManager.instance.GetTile(transform.position).myTile.myOccupant = this;
         }
@@ -26,23 +28,31 @@ public class AddPolution : MonoBehaviour {
         EcoManager.instance.AddPollution(-pollution);
     }
 
-    public void Burn(){
-        if(canBurn == true){
+    public void Burn() {
+        if (canBurn == true) {
             burning = true;
         }
     }
 
-    public IEnumerator Burning(){
+    public IEnumerator Burning() {
         myResource.myResource = burnedResource;
-        float time = Random.Range(burnMin,burnMax);
+        float time = Random.Range(burnMin, burnMax);
+        float baseTime = time;
         float wait = time;
+        myPartical.SetActive(true);
 
-        while(time <= 0){
+        while (time <= 0) {
             time -= Time.deltaTime;
-            if(Random.Range(1,100) == 1){
+            myRenderer.material.color = myRenderer.material.color - new Color(255 * time / baseTime, 255 * time / baseTime, 255 * time / baseTime, 255 * time / baseTime);
+
+            if (Random.Range(1, 1000)== 1) {
                 EcoManager.instance.StartFire(transform.position);
             }
             yield return null;
         }
+
+        Destroy(toDestroy);
+
+        myPartical.SetActive(false);
     }
 }
